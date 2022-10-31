ceb52009818735a45cc7b5ea524f9a25c841f7e1
RikuOta <otarikuweb@gmail.com>
2022-10-31 23:33:17 +0900

    chore: Mantine と Tailwind CSS の設定を合わせる

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
+import 'styles/globals.css'
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { MantineProvider } from '@mantine/core';
+import { MantineProvider, MantineThemeOverride} from '@mantine/core';
+import resolveConfig from 'tailwindcss/resolveConfig'
+import tailwindConfig from '../tailwind.config.js'
+
+const resolvedTailwindConfig = resolveConfig(tailwindConfig)
+
+// Tailwind のブレイクポイントを Mantine 用に変換
+const tailwindScreensForMantine: {[key: string]: number} = {}
+const tailwindScreens = resolvedTailwindConfig.theme?.screens
+if (tailwindScreens) {
+  for (const [key, value] of Object.entries(tailwindScreens)) {
+    // 末尾の px を削除してから数値に変換
+    tailwindScreensForMantine[key] = Number(value.slice(0, -2))
+  }
+}
+
+const theme: MantineThemeOverride = {
+  breakpoints: tailwindScreensForMantine
+}
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
@@ -15,10 +34,7 @@ export default function App(props: AppProps) {
       <MantineProvider
         withGlobalStyles
         withNormalizeCSS
-        theme={{
-          /** Put your mantine theme override here */
-          colorScheme: 'light',
-        }}
+        theme={theme}
       >
         <Component {...pageProps} />
       </MantineProvider>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,12 @@
+// Mantine のデフォルトカラーを Tailwind 用に変換
+const { DEFAULT_THEME } = require('@mantine/styles')
+const mantineColors = {}
+for (const [name, colors] of Object.entries(DEFAULT_THEME.colors)) {
+  mantineColors[name] = colors.reduce((acc, cur, i) => {
+    return { ...acc, [i]: cur }
+  }, {})
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,7 +14,10 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    colors: mantineColors
   },
   plugins: [],
+  corePlugins: {
+    preflight: false,
+  }
 }
